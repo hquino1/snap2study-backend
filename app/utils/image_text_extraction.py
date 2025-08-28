cc155b2c8c5ec18cb04d24caa7a641109c0ba890
Hector Quinones <quinonhector1@gmail.com>
2025-08-28 13:36:18 -0500

    black pre commit test

--- app/utils/image_text_extraction.py
+++ app/utils/image_text_extraction.py
@@ -2,14 +2,8 @@ from io import BytesIO
 import pytesseract
 import base64
 from PIL import Image
-
-
 def image_text_extraction(image):
-
     image_decoded = base64.b64decode(image)
-
     image = Image.open(BytesIO(image_decoded))
-
     text = pytesseract.image_to_string(image)
-
     return text
